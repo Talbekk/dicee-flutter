1355f4e83c751b924a3e3b02d7126c6230aaa900
Talbekk <mt.campbell@outlook.com>
2022-03-05 19:24:43 +0000

    adds basic btn to each dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   return runApp(
@@ -23,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Pressed dice 1');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Pressed dice 2');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
